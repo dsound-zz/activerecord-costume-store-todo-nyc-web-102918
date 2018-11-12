caa39ab6a74476ea2855d53604b16748ea18cc39
dsound-zz <demiansims@gmail.com>
2018-11-12 09:47:34 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -17,4 +17,9 @@ DB = ActiveRecord::Base.connection
 
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
+
+#   ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/artists.sqlite"
+# )
 end
